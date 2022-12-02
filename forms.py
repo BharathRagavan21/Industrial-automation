ad080f5a575e0c6f817856ba7ff62fc128b76d55
BharathRagavan21 <bharathragavan21@gmail.com>
2022-12-02 21:47:14 +0530

    Initial commit

---
+++ forms.py
@@ -0,0 +1,21 @@
+from flask_wtf import FlaskForm 
+from wtforms import StringField, SelectField, PasswordField, SubmitField, IntegerField 
+from wtforms.validators import DataRequired, Length, EqualTo, ValidationError
+
+class RegistrationForm(FlaskForm): 
+    username = StringField("Username", validators=[ DataRequired(),Length(min=2, max=20)]) 
+
+group = SelectField("Group", validators=[DataRequired()], choices=["staff", "admin"]) 
+password = PasswordField("Password", validators=[DataRequired()]) 
+submit = SubmitField("Create")
+
+class LoginForm(FlaskForm): 
+    username = StringField("Username", validators=[ DataRequired(),Length(min=2, max=20)])
+
+    password = PasswordField("Password", validators=[DataRequired()]) 
+    submit = SubmitField("Login") 
+class AddBotForm(FlaskForm): 
+    id = IntegerField("Bot Id", validators=[DataRequired()])
+
+type = SelectField("Type", validators=[DataRequired()], choices=["Forklift", "Mover"]) 
+submit = SubmitField("Add")
